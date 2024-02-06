1d6fcdf870a02e532cf3a3473361495ca9225c75
xgsarah <sjbquimpo@gmail.com>
2024-02-06 21:47:34 +0800

    refactor and fix populateDisplay

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let operator = null;
 let display = 0;
 
 function add(arrNum) {
-  console.log('ADD:', arrNum);
   return arrNum.reduce((prev, curr) => prev + curr, 0);
 }
 
@@ -38,11 +37,10 @@ function operate(num1, num2, operator) {
   }
 }
 
-function populateDisplay(str) {
-  console.log('disp:', str);
-  display = display ? String(display).concat(str) : str;
-  const displayDiv = document.querySelector('.display');
+function populateDisplay(value) {
+  display = display ? (display += value) : value;
 
+  const displayDiv = document.querySelector('.display');
   displayDiv.innerText = display;
 }
 
@@ -52,19 +50,7 @@ function handleOperationButtons() {
     el.addEventListener('click', (e) => {
       operator = e.target.innerText;
 
-      if (!num1) {
-        num1 = display;
-      } else if (!num2) {
-        num2 = display;
-      }
-
-      if (num1 && num2) {
-        num1 = operate(num1, num2, operator);
-        num2 = 0;
-        display = 0;
-        populateDisplay(num1);
-      }
-      //   console.log({ num1, num2, operator });
+      num1 = display;
       display = 0;
     })
   );
@@ -73,17 +59,15 @@ function handleOperationButtons() {
 function handleEqualButton() {
   const equalButton = document.querySelector('.equal');
   equalButton.addEventListener('click', (e) => {
-    num2 = !num2 ? display : 0;
-    display = 0;
+    if (num1) {
+      num2 = display;
+    } else {
+      num1 = display;
+    }
 
-    console.log({ num1, num2, operator });
+    display = 0;
     const result = operate(num1, num2, operator);
-    display = 0;
     populateDisplay(result);
-
-    num1 = 0;
-    num2 = 0;
-    operator = null;
   });
 }
 
