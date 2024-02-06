c541f2add0dbfdac4229ea77a36c4de55c68ed80
xgsarah <sjbquimpo@gmail.com>
2024-02-06 19:08:04 +0800

    implement operate function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const operators = ['add', 'subtract', 'divide', 'multiply'];
+const operators = ['add', 'subtract', 'multiply', 'divide'];
 
 function add(arrNum) {
   return arrNum.reduce((prev, curr) => prev + curr, 0);
@@ -15,4 +15,19 @@ function multiply(arrNum) {
 
 function divide(arrNum) {
   return arrNum.reduce((prev, curr) => prev / curr);
+}
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case operators[0]:
+      return add([num1, num2]);
+    case operators[1]:
+      return subtract([num1, num2]);
+    case operators[2]:
+      return multiply([num1, num2]);
+    case operators[3]:
+      return divide([num1, num2]);
+    default:
+      return 0;
+  }
 }
