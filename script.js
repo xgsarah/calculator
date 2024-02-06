ba2fdf20b809dae2a4a426b9c562ba2f8c636cf9
xgsarah <sjbquimpo@gmail.com>
2024-02-06 19:03:29 +0800

    create numbers and operators variable

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function add(numbers) {
-  return numbers.reduce((prev, curr) => prev + curr, 0);
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operators = ['add', 'subtract', 'divide', 'multiply'];
+
+function add(arrNum) {
+  return arrNum.reduce((prev, curr) => prev + curr, 0);
 }
 
-function subtract(numbers) {
-  return numbers.reduce((prev, curr) => prev - curr);
+function subtract(arrNum) {
+  return arrNum.reduce((prev, curr) => prev - curr);
 }
 
-function multiply(numbers) {
-  return numbers.reduce((prev, curr) => prev * curr);
+function multiply(arrNum) {
+  return arrNum.reduce((prev, curr) => prev * curr);
 }
 
-function divide(numbers) {
-  return numbers.reduce((prev, curr) => prev / curr);
+function divide(arrNum) {
+  return arrNum.reduce((prev, curr) => prev / curr);
 }
