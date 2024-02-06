b80efb6f93b4bd01d978f6030701ff228cc29290
xgsarah <sjbquimpo@gmail.com>
2024-02-06 19:00:19 +0800

    implement operators

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+function add(numbers) {
+  return numbers.reduce((prev, curr) => prev + curr, 0);
+}
+
+function subtract(numbers) {
+  return numbers.reduce((prev, curr) => prev - curr);
+}
+
+function multiply(numbers) {
+  return numbers.reduce((prev, curr) => prev * curr);
+}
+
+function divide(numbers) {
+  return numbers.reduce((prev, curr) => prev / curr);
+}
